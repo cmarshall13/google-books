3c56d39e7b4f6e2a492eb9b01d589c1a94df25ca
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2021-12-19 20:47:39 -0500

    made changes to server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
-const {authMiddleware} = require('./utils/auth');
-const {typeDefs, resolvers} = require('./schemas');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+
 const server = new ApolloServer({ 
   typeDefs, 
   resolvers, 
@@ -24,6 +26,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
